1e8ceff10da164f751d93da884a636b47976541f
flyi <im@flynn.xyz>
2023-10-05 20:39:47 +0800

    libInjectCss

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import vue from "@vitejs/plugin-vue";
 import * as path from "path";
 import typescript2 from "rollup-plugin-typescript2";
 import dts from "vite-plugin-dts";
+import { libInjectCss, scanEntries } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    libInjectCss(),
     dts({
       insertTypesEntry: true,
     }),
